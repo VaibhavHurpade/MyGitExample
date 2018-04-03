de4c70f758f1c7cea6f2dad4eba6f5afd96e6996
Vaibhav Hurpade <vaibhavhurpade80@gmail.com>
2018-04-03 16:59:00 +0530

    Initial commit

---
+++ app/src/main/java/collegemgnt/example/com/mygitexample/MainActivity.java
@@ -0,0 +1,13 @@
+package collegemgnt.example.com.mygitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
